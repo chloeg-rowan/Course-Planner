5f8cada3e87209234a17882cdf0eabf37feb827c
chloeg-rowan <c.garvey.2023@gmail.com>
2026-07-18 16:37:43 -0400

    Initial Spring Boot scaffold.

---
+++ src/test/java/com/garvey/course_planner/CoursePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garvey.course_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
